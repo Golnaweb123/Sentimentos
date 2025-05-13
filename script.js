dbacc6d599992fe514c63a9a3c68aa43d92b38fe
Golnaweb123 <147342806+Golnaweb123@users.noreply.github.com>
2025-05-13 15:56:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function handleSubmit(event) {
+            event.preventDefault(); // Prevent form submission
+            
+            document.body.innerHTML = "";
+            const hackedMessage = document.createElement("div");
+            hackedMessage.classList.add("hacked");
+            hackedMessage.innerText = "Acesso garantido\nHackeando sistema...\nBaixando data do usuário...";
+            document.body.appendChild(hackedMessage);
+            
+            let count = 0;
+            const interval = setInterval(() => {
+                hackedMessage.innerText += "\nERROR: SYSTEM OVERRIDE!";
+                count++;
+                if (count > 3) {
+                    clearInterval(interval);
+                    setTimeout(() => {
+                        alert("Peguei seus dados se fodakkkkkkkkkkkk");
+                        location.reload();
+                    }, 2000);
+                }
+            }, 1000);
+    
+        }
 document.addEventListener("DOMContentLoaded", () => {
     const sky = document.getElementById("sky");   
     const cloudNum = 25;
